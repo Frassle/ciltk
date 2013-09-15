c90fa69e67f5a10b97153893b57cff37af87dc48
Fraser <frassle@gmail.com>
2013-09-15 14:56:57 +0100

    Documentation for Leave

--- CilTK/Cil.cs
+++ CilTK/Cil.cs
@@ -902,6 +902,29 @@ namespace Silk
         public static void Ldstr(string str) { throw new Exception("CilTK Rewriter not run."); }
         public static void Ldtoken() { throw new Exception("CilTK Rewriter not run."); }
         public static void Ldvirtftn() { throw new Exception("CilTK Rewriter not run."); }
+
+        /// <summary>
+        /// Exit a protected region of code.
+        /// 
+        /// Stack Transition:
+        /// ..., -> 
+        /// </summary>
+        /// <remarks>
+        /// The leave instruction unconditionally transfers control to target. target is represented as a signed
+        /// offset (4 bytes for leave, 1 byte for leave.s) from the beginning of the instruction following the
+        /// current instruction.
+        /// The leave instruction is similar to the br instruction, but the former can be used to exit a try,
+        /// filter, or catch block whereas the ordinary branch instructions can only be used in such a
+        /// block to transfer control within it. The leave instruction empties the evaluation stack and ensures
+        /// that the appropriate surrounding finally blocks are executed.
+        /// It is not valid to use a leave instruction to exit a finally block. To ease code generation for
+        /// exception handlers it is valid from within a catch block to use a leave instruction to transfer
+        /// control to any instruction within the associated try block.
+        /// The leave instruction can be used to exit multiple nested blocks (see Partition I).
+        /// If an instruction has one or more prefix codes, control can only be transferred to the first of these
+        /// prefixes.
+        /// </remarks>
+        /// <param name="label">The label to branch to.</param>
         public static void Leave(string label) { throw new Exception("CilTK Rewriter not run."); }
 
         /// <summary>
