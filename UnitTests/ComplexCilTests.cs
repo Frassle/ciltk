514553ac4c1afe2b2e1b24becb053ce4af829070
Fraser <frassle@gmail.com>
2013-12-09 23:28:39 +0000

    Some more complex tests.
    Tests of methods that we expect to be used.

---
+++ UnitTests/ComplexCilTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Silk;
+using System.Runtime.InteropServices;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ComplexCilTests
+    {
+        static void CopyTo<T>(IntPtr dst, T value)
+        {
+            Cil.Ldarg(0);
+            Cil.Ldarga(1);
+            Cil.Sizeof<T>();
+            Cil.Cpblk();
+        }
+
+        [TestMethod]
+        public void TestCopy()
+        {
+            TestStruct[] destination = new TestStruct[1];
+            GCHandle handle = GCHandle.Alloc(destination, GCHandleType.Pinned);
+            IntPtr dst = handle.AddrOfPinnedObject();
+
+            TestStruct value = new TestStruct() 
+            {
+                A = 1, B = 2,
+            };
+
+            CopyTo(dst, value);
+
+            Assert.AreEqual(value, destination[0]);
+        }
+
+        static uint Sizeof<T>()
+        {
+            Cil.Sizeof<T>();
+            Cil.Ret();
+
+            return 0;
+        }
+
+        [TestMethod]
+        public void TestSizeof()
+        {
+            Assert.AreEqual(4, Sizeof<int>(), "int");
+            Assert.AreEqual(8, Sizeof<long>(), "long");
+            Assert.AreEqual(8, Sizeof<TestStruct>(), "TestStruct");
+            Assert.AreEqual(IntPtr.Size, Sizeof<TestClass>(), "TestClass");
+        }
+    }
+}
