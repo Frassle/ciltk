b971f1b696220bb4523b2ba534dd703c8d22d575
Fraser <frassle@gmail.com>
2014-02-11 23:24:08 +0000

    Info by name.
    Don't use fancy overload tricks.

---
+++ CilTK/Info.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silk
+{
+    public static class Info
+    {
+        public static System.Reflection.ParameterInfo Parameter(string parameter)
+        {
+            throw new Exception("CilTK Rewriter not run.");
+        }
+
+        public static System.Reflection.LocalVariableInfo Variable(string variable)
+        {
+            throw new Exception("CilTK Rewriter not run.");
+        }
+
+        public static System.Reflection.FieldInfo Field(string field)
+        {
+            throw new Exception("CilTK Rewriter not run.");
+        }
+
+        public static System.Reflection.PropertyInfo Property(string property)
+        {
+            throw new Exception("CilTK Rewriter not run.");
+        }
+
+        public static System.Reflection.MethodInfo Method(string method)
+        {
+            throw new Exception("CilTK Rewriter not run.");
+        }
+    }
+}
