33056716239b709706d123c85512eb18fd3a2827
Fraser <frassle@gmail.com>
2014-02-16 21:40:50 +0000

    Info test.

---
+++ UnitTests/InfoTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class InfoTests
+    {
+        [TestMethod]
+        public void TestVariable()
+        {
+            int a = 0;
+            var ainfo = Silk.Info.Variable("a");
+            Assert.AreEqual(0, ainfo.LocalIndex);
+            Assert.AreEqual(typeof(int), ainfo.LocalType);
+            Silk.Cil.KeepAlive(a);
+        }
+
+        [TestMethod]
+        public void TestField()
+        {
+            var info = Silk.Info.Field("UnitTests.TestStruct::A");
+            Assert.AreEqual("A", info.Name);
+            Assert.AreEqual(typeof(int), info.FieldType);
+        }
+    }
+}
